a65de9e47a5a1f5c2203f97195fc36fb1ed4f46b
Jon Flores <jonathanflores@u.boisestate.edu>
2024-02-25 13:47:04 -0700

    Username get route implemented

--- src/main/java/cs208/PracticeController.java
+++ src/main/java/cs208/PracticeController.java
@@ -1,6 +1,7 @@
 package cs208;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,10 @@ public class PracticeController
             @RequestParam("last_name") String last_name
     )
     {
-        System.out.println("TutorialController.getSearchWithTwoParameters - START");
+        System.out.println("PracticeController.getSearchWithTwoParameters - START");
         System.out.println("parameter1 sent in the browser URL = " + first_name);
         System.out.println("parameter2 sent in the browser URL = " + last_name);
-        System.out.println("TutorialController.getSearchWithTwoParameters - END");
+        System.out.println("PracticeController.getSearchWithTwoParameters - END");
 
         String valueReturnedToBrowser =
                 "These are the values of parameters passed in the URL:<br>" +
@@ -25,13 +26,29 @@ public class PracticeController
                         "parameter2 = " + last_name + "<br><br>" +
                         "NOTE: the parameters are separated in the URL by an ampersand (&amp;) character.<br><br>" +
                         "<hr>" +
-                        "Find me in the TutorialController.getSearchWithTwoParameters() method";
+                        "Find me in the PracticeController.getSearchWithTwoParameters() method";
 
         return valueReturnedToBrowser;
     }
 
 
     // TODO: create a GET route with a path parameter
+    @GetMapping("/user/{userName}/profile")
+    String getProfileWithStringPathVariableCalledBSUUsername(@PathVariable("userName") String userName)
+    {
+        System.out.println("PracticeController.getIssuesWithStringPathVariableCalledIssueId - START");
+        System.out.println("Parameter received in the path of the URL:");
+        System.out.println("BSUUsername = " + userName);
+        System.out.println("PracticeController.getIssuesWithStringPathVariableCalledBSUUsername - END");
+
+        String valueReturnedToBrowser =
+                "Parameter received in the path of the URL:<br>" +
+                        "userName = " + userName + "<br><br>" +
+                        "<hr>" +
+                        "Find me in the PracticeController.getProfileWithStringPathVariableCalledBSUUsername() method";
+
+        return valueReturnedToBrowser;
+    }
 
 
     // TODO: create a POST route with two form parameters
